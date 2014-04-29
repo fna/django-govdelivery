fd299b2d2cadd98abd701a4a2835c43306e3be81
S Gorobet <Serghei.Gorobet@cfpb.gov>
2014-04-29 16:40:47 -0400

    change urls

--- django_govdelivery/urls.py
+++ django_govdelivery/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import *
 
 
-urlpatterns = patterns('django_govdelivery.views',
-    url(r'^new$', 'subscribe'),
+urlpatterns = patterns(
+    'django_govdelivery.views',
+    url(r'^new/$', 'subscribe'),
 )
